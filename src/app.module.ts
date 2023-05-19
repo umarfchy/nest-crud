c5706191c16d02b27229c7e7c00beb41767d1ca2
umarfchy <umarfchy@gmail.com>
2023-05-20 01:02:42 +0600

    add mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
 import { TodosModule } from './todos/todos.module';
 
 @Module({
-  imports: [UsersModule, TodosModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://root:example@localhost:8000/nestdb'),
+    UsersModule,
+    TodosModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
